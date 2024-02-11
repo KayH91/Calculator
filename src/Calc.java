7e8c509b380e39c7d126aacb7dfabe910b3325b9
Admin <kayh.martirosyan@gmail.com>
2024-02-11 12:59:06 +0400

    123

--- src/Calc.java
+++ src/Calc.java
@@ -10,7 +10,7 @@ class Main {
         String op = parts[1];
 
         if (a < 1 || a > 10 || b < 1 || b > 10) {
-            throw new IllegalArgumentException("Числа должны быть в диапазоне от 1 до 10");
+            throw new IllegalArgumentException("Числа должны быть в диапазоне от 0 до 10");
         }
 
         int result;
